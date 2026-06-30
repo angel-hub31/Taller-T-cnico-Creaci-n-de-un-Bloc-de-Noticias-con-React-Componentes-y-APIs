d7dac52a500a4bcc2e1ebad177a7c8fa58d1b7fe
angel-hub31 <moralesmales1@hotmail.com>
2026-06-30 15:47:15 -0500

    Evidencias de capturas

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,15 @@ import Header from './components/Header';
 import ListaNoticias from './components/ListaNoticias';
 import './index.css';
 
-
 function App() {
-
-
   return (
-
     <div className="App">
-
-
         <Header />
-
-
         <main>
-
             <ListaNoticias />
-
         </main>
-
-
     </div>
-
   );
-
 }
 
 
--- src/components/TarjetaNoticia.jsx
+++ src/components/TarjetaNoticia.jsx
@@ -1,46 +1,24 @@
 import '../index.css';
-
-
 function TarjetaNoticia({info}) {
 
-
     const leerMas = () => {
-
         alert(
             `Leyendo noticia:\n\n${info.title}`
         );
-
     }
 
-
-
     return (
-
         <article className="card">
-
-
             <h3>
                 {info.title}
             </h3>
-
-
             <p>
                 {info.body}
             </p>
-
-
-
             <button onClick={leerMas}>
                 Leer más
             </button>
-
-
-
         </article>
-
     );
-
 }
-
-
 export default TarjetaNoticia;
